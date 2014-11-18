8e9a084e687ca002d993dbdd397b58a538c170eb
SuperOxigen <SuperOxigen@users.noreply.github.com>
2014-11-18 09:24:55 -0500

    MoveAction.java
    This holds the information for each individual move to be send from one client to the other.

---
+++ MoveAction.java
@@ -0,0 +1,60 @@
+import java.sql.Time;
+
+// line 207 "model.ump"
+// line 253 "model.ump"
+public class MoveAction extends Action {
+
+	// ------------------------
+	// MEMBER VARIABLES
+	// ------------------------
+
+	// MoveAction Attributes
+	private int x;
+	private int y;
+
+	// ------------------------
+	// CONSTRUCTOR
+	// ------------------------
+
+	public MoveAction(Time aTime, int aX, int aY) {
+		super(aTime);
+		x = aX;
+		y = aY;
+	}
+
+	// ------------------------
+	// INTERFACE
+	// ------------------------
+
+	public boolean setX(int aX) {
+		boolean wasSet = false;
+		x = aX;
+		wasSet = true;
+		return wasSet;
+	}
+
+	public boolean setY(int aY) {
+		boolean wasSet = false;
+		y = aY;
+		wasSet = true;
+		return wasSet;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void delete() {
+		super.delete();
+	}
+
+	public String toString() {
+		String outputString = "";
+		return super.toString() + "[" + "x" + ":" + getX() + "," + "y" + ":"
+				+ getY() + "]" + outputString;
+	}
+}
